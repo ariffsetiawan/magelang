32aa02c78becb2ddaaa42d3b06f2e8fd452423a8
Arif Setiawan <ariffsetiawan@gmail.com>
2024-10-07 23:01:57 +0700

    fix: responsive menu

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -9,26 +9,31 @@
 
 ?>
 
-<header id="masthead" class="bg-black text-white flex justify-between items-center p-8 border-t-magenta border-t-4 shadow">
+<header id="masthead" class="bg-black text-white flex flex-col md:flex-row justify-between items-center p-8 border-t-magenta border-t-4 shadow text-center md:text-left">
 
 	<div class="text-lg">
-		<h1 class="text-3xl font-extrabold"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<h1 class="text-3xl font-extrabold mb-1"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 		<?php
 		$magelang_description = get_bloginfo( 'description', 'display' );
 		if ( $magelang_description || is_customize_preview() ) :
-			?>
-			<p class="text-sm"><?php echo $magelang_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+		?>
+		<p class="text-sm"><?php echo $magelang_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 		<?php endif; ?>
 	</div>
 
-	<nav id="site-navigation" aria-label="<?php esc_attr_e( 'Main Navigation', 'magelang' ); ?>">
-		<button aria-controls="primary-menu" aria-expanded="false" class="hidden"><?php esc_html_e( 'Primary Menu', 'magelang' ); ?></button>
-
+	<nav id="site-navigation" class="mt-4 md:mt-0" aria-label="<?php esc_attr_e( 'Main Navigation', 'magelang' ); ?>">
+		<button class="sm:hidden" aria-controls="primary-menu" aria-expanded="false">
+			<svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+				<title>Mobile menu</title>
+				<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+			</svg>
+		</button>
 		<?php
 		wp_nav_menu(
 			array(
 				'theme_location' => 'menu-1',
 				'menu_id'        => 'primary-menu',
+				'menu_class'	 =>	'list-reset hidden sm:flex text-lg sm:li-pl-4 li-pb-4 md:li-pb-0',
 				'items_wrap'     => '<ul id="%1$s" class="%2$s" aria-label="submenu">%3$s</ul>',
 			)
 		);
